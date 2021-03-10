6b2b7c264eaac703634df3dca8ae4052a8133bf7
Rodolfo Camacho <66486204+RodolfoCamachoOn@users.noreply.github.com>
2021-03-10 15:27:54 -0600

    Camare Logic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { PhotoService } from '../services/photo.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public photoService: PhotoService) { }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 
 }
